d515a551cb638948ece7626e2d206fd7b4410531
camargo <camargo.cac@gmail.com>
2016-03-14 10:44:07 -0700

    update libs

--- app/src/ts/boot.ts
+++ app/src/ts/boot.ts
@@ -1,5 +1,8 @@
 // Info about boot.ts: https://angular.io/docs/ts/latest/quickstart.html#!#boot
 
+// ISSUE: https://github.com/angular/angular/blob/master/CHANGELOG.md#200-beta6-2016-02-11
+///<reference path="../../../node_modules/angular2/typings/browser.d.ts"/>
+
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,39 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
+// Application files.
+var appFiles = 'app/src/ts/*.ts';
+
 // List of .js libs.
-var jsLibFiles = [
+var angular2Files = [
+    'node_modules/es6-shim/es6-shim.min.js',
+    'node_modules/systemjs/dist/system-polyfills.js',
     'node_modules/angular2/bundles/angular2-polyfills.js',
     'node_modules/systemjs/dist/system.src.js',
     'node_modules/rxjs/bundles/Rx.js',
     'node_modules/angular2/bundles/angular2.dev.js',
     'node_modules/angular2/bundles/router.dev.js',
-    'node_modules/angular2/bundles/http.dev.js',
-    'node_modules/material-design-lite/material.min.js'
+    'node_modules/angular2/bundles/http.dev.js'
 ];
 
 // List of .css libs.
-var cssLibFiles = [
-    'node_modules/material-design-lite/material.min.css',
+var cssFiles = [
     'app/styles/angular2crud.css'
 ];
 
 // Concat all .js libs.
-gulp.task('js', function() {
-    return gulp.src(jsLibFiles)
-               .pipe(concat('angular2crud.js'))
+gulp.task('concat-angular', function() {
+    return gulp.src(angular2Files)
+               .pipe(concat('angular2.js'))
                .pipe(gulp.dest('./dist'));
 });
 
 // Concat all .css libs.
-gulp.task('css', function() {
-    return gulp.src(cssLibFiles)
+gulp.task('concat-css', function() {
+    return gulp.src(cssFiles)
                .pipe(concat('angular2crud.css'))
                .pipe(gulp.dest('./dist'));
 });
 
 // Default build task (run by typing 'gulp').
-gulp.task('default', ['js', 'css']);
+gulp.task('default', ['concat-angular', 'concat-css']);
